4dec3dfc5bf7a7a78b40a266d04ca838c6ca8c58
Jacob <jacobkdavis91@yahoo.com>
2023-08-31 12:30:48 -0400

    adding files

---
+++ test/java/dev/davis/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.davis.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
